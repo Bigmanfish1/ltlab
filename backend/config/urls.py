e6226e0958fe51343a2405965323a84be091e71f
Bigmanfish1 <igbojinnavictor@gmail.com>
2026-06-04 23:19:46 +0200

    Added sandbox and countere example pages

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
+from apps.checker.views import counterexample, verify_ltl
 from config.api import api
-from config.views import home
+from config.views import home, sandbox
 
 urlpatterns = [
     path("", home, name="home"),
+    path("sandbox/", sandbox, name="sandbox"),
+    path("sandbox/verify/", verify_ltl, name="sandbox_verify"),
+    path("sandbox/counterexample/", counterexample, name="sandbox_counterexample"),
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
--- backend/config/views.py
+++ backend/config/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+
+def sandbox(request):
+    return render(request, "sandbox/sandbox.html")
